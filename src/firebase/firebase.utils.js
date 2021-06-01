ddde587ce87e0f6fc62888378fb55d1e49f61bc7
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2021-06-01 18:57:25 +0100

    Added firebase setup file and sign in with Google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyAORgxesTgoDfdZDqf_VY1iEzyTaRDohbQ",
+  authDomain: "camera-shop-db.firebaseapp.com",
+  projectId: "camera-shop-db",
+  storageBucket: "camera-shop-db.appspot.com",
+  messagingSenderId: "80115904774",
+  appId: "1:80115904774:web:e7ada12b2512b713d5ee87",
+  measurementId: "G-KVVK1MMT8P"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
