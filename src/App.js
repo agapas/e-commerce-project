0cce97137e231a3515853ca0b2495fee37fbadf3
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2021-06-01 11:52:03 +0100

    Code clenaup and small changes in styles

--- src/App.js
+++ src/App.js
@@ -7,30 +7,6 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import { Header } from './components/header/header.component';
 
-// const DslrCamerasPage = () => (
-//   <h1>Digital SLR Cameras</h1>
-// );
-
-// const MirrorlessCamerasPage = () => (
-//   <h1>Mirrorless Cameras</h1>
-// );
-
-// const BridgeCamerasPage = () => (
-//   <h1>Bridge Cameras</h1>
-// );
-
-// const CompactCamerasPage = () => (
-//   <h1>Compact Cameras</h1>
-// );
-
-// const InstantCamerasPage = () => (
-//   <h1>Instant Cameras</h1>
-// );
-
-// const ActionCamerasPage = () => (
-//   <h1>GoPro & Action Cameras</h1>
-// );
-
 function App() {
   return (
     <div>
@@ -38,12 +14,6 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-        {/* <Route path='/dslr' component={DslrCamerasPage} />
-        <Route path='/mirrorless' component={MirrorlessCamerasPage} />
-        <Route path='/bridge' component={BridgeCamerasPage} />
-        <Route path='/compact' component={CompactCamerasPage} />
-        <Route path='/instant' component={InstantCamerasPage} />
-        <Route path='/action' component={ActionCamerasPage} /> */}
       </Switch>
     </div>
   );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -6,7 +6,6 @@ import './homepage.styles.scss';
 
 const HomePage = () => (
   <div className='homepage'>
-    {/* <div className='name'>Camera Shop</div> */}
     <Directory />
   </div>
 );
