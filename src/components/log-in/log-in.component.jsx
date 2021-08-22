3db1a1fb2307eee2f5762818d5c38ee16379a889
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2021-08-22 16:04:06 +0100

    'Sign in with Google' fix to not activate autocoplete of form inputs

--- src/components/log-in/log-in.component.jsx
+++ src/components/log-in/log-in.component.jsx
@@ -59,7 +59,11 @@ export class LogIn extends React.Component {
           />
           <div className='buttons-container'>
             <CustomButton type='submit'>Sign in</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type='button'
+              onClick={signInWithGoogle}
+              isGoogleSignIn
+            >
               Sign in with Google
             </CustomButton>
           </div>
