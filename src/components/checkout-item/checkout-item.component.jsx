aee8ae8c6f6764fec2e57713a615eb3218343e12
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2021-10-03 14:02:06 +0100

    Added logic for addItem and removeItem into CheckoutItem component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { clearItemFromCart } from '../../redux/cart/cart.actions';
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { name, imageDirectory, price, quantity } = cartItem;
   return (
     <div className='checkout-item'>
@@ -13,18 +13,31 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
         <img src={`${imageDirectory}/1.jpg`} alt='item' />
       </div>
       <div className='name'>{name}</div>
-      <div className='quantity'>{quantity}</div>
+      <div className='quantity'>
+        <div
+          className='arrow'
+          onClick={() => removeItem(cartItem)}
+        >
+          &#10094;
+        </div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
+      </div>
       <div className='price'>{price}</div>
       <div
         className='remove-button'
         onClick={() => clearItem(cartItem)}
-      >&#10005;</div>
+      >
+        &#10005;
+      </div>
     </div>
   );
 };
 
 const mapDispatchToProps = dispatch => ({
-  clearItem: item => dispatch(clearItemFromCart(item))
+  clearItem: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item)),
 });
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
