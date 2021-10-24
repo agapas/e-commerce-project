2ce46f5937e98325c9c0d6847edf4a42557411fb
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2021-10-24 17:32:26 +0100

    Used sessionStorage instead of local storage to fix issue on github pages

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,19 +1,19 @@
 import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import sessionStorage from "redux-persist/lib/storage/session";
 
 import { userReducer } from './user/user.reducer';
 import { cartReducer } from './cart/cart.reducer';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['cart'],
+const cartItemsConfig = {
+  key: 'cart',
+  storage: sessionStorage,
+  whitelist: ['cartItems'],
 };
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer,
+  cart: persistReducer(cartItemsConfig, cartReducer),
 });
 
-export default persistReducer(persistConfig, rootReducer);
+export default rootReducer;
